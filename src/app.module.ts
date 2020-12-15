eae54d59dc9220a590ee554dc317336949cd243f
Giordano <giordano200699@gmail.com>
2020-12-15 02:37:13 -0500

    Ajuste: Ajustando para subir a Heroku parte 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { SprintsModule } from './componentes/sprints/sprints.module';
-import { info } from 'env.js'
+// import { info } from 'env.js'
+
+var info = {
+	'DATABASE_ROOT': 'mongodb+srv://giordano:waldo@cluster0-p2txm.mongodb.net/BackendOloresCodigo?retryWrites=true'
+}
 
 @Module({
   imports: [MongooseModule.forRoot(info.DATABASE_ROOT,{useNewUrlParser: true}),
